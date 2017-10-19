82867b98fb0811ea3d3fb22f474bb7b15af686b7
serhaterfidan <serhat@elmasteknoloji.com.tr>
2017-10-19 15:32:33 +0300

    Uygulama kilitleyicisi düzenlendi

---
+++ app/src/main/java/com/colornative/lockapp/VersionKeys.java
@@ -0,0 +1,10 @@
+package com.colornative.lockapp;
+
+abstract class VersionKeys {
+
+	// Careful to not change this from one version to another
+	/**
+	 * Whether ads are forced from the server or not.
+	 */
+	public static final String ENABLE_ADS = "com.colornative.option.1";
+}
